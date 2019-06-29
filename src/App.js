abc425bffbd5535b23dd0c2e8a3f732a696cd761
José Silva <140221057@estudantes.ips.pt>
2019-06-29 23:31:52 +0100

    Loader add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './components/content/About';
 import Partner from './components/management/Partner';
 import Circuit from './components/management/Circuit';
 import Contacts from './components/content/Contacts';
-import Loader from './components/test/Loader';
+import Loader from './loader.txt';
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
           <Route path='/circuits' component={Circuit} />
           <Route path='/contacts' component={Contacts} />
           <Route path='/contacts' component={Contacts} />
+          
           <Route path='/loaderio-8cf6c0f47276abcf129b98f5691e13e0' component={Loader} />
         </Switch>
       </div>
