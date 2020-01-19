4c7a8b7567e06d883441fbf947f49fca00fa09dd
José Silva <140221057@estudantes.ips.pt>
2020-01-19 02:21:51 +0000

    Removed robots route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from './components/content/About';
 import Partner from './components/management/Partner';
 import Circuit from './components/management/Circuit';
 import Contacts from './components/content/Contacts';
-import Robots from './robots.txt';
 import Loader from './loader.txt';
 
 function App() {
@@ -21,9 +20,7 @@ function App() {
           <Route path='/partners' component={Partner} />
           <Route path='/circuits' component={Circuit} />
           <Route path='/contacts' component={Contacts} />
-          <Route path='/contacts' component={Contacts} />
-          <Route path='/robots.txt' component={Robots} />
-
+          
           <Route path='/loaderio-8cf6c0f47276abcf129b98f5691e13e0' component={Loader} />
         </Switch>
       </div>
